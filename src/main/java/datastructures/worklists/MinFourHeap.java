e2c320c07fb29641700fdf046e963b4fcc8a8225
Ethan <ebai2022@cs.washington.edu>
2023-05-04 16:18:06 -0700

    first part of (7) done

--- src/main/java/datastructures/worklists/MinFourHeap.java
+++ src/main/java/datastructures/worklists/MinFourHeap.java
@@ -4,6 +4,7 @@ import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.worklists.PriorityWorkList;
 
 import java.util.Comparator;
+import java.util.NoSuchElementException;
 
 /**
  * See cse332/interfaces/worklists/PriorityWorkList.java
@@ -12,38 +13,127 @@ import java.util.Comparator;
 public class MinFourHeap<E> extends PriorityWorkList<E> {
     /* Do not change the name of this field; the tests rely on it to work correctly. */
     private E[] data;
+    private int size;
+    private int capacity;
+    private Comparator<E> compareType;
 
     public MinFourHeap(Comparator<E> c) {
-        throw new NotYetImplementedException();
+        capacity = 22;
+        data = (E[])new Object[capacity];
+        size = 0;
+        compareType = c;
     }
 
     @Override
     public boolean hasWork() {
-        throw new NotYetImplementedException();
+        if (size < 1){
+            return false;
+        }
+        return true;
     }
 
     @Override
     public void add(E work) {
-        throw new NotYetImplementedException();
+        if (size == data.length-1){
+            //amoritized O(1) resize
+            capacity = capacity * 2;
+            E[] copy = (E[])new Object[capacity];
+            for (int i = 0; i < size+1; i++){
+                copy[i] = data[i];
+            }
+            data = copy;
+        }
+        size++;
+        // place at "size-1" (0 indexed) and then check where it should belong
+        int i = percolateUp(size - 1,work);
+        data[i] = work;
     }
 
     @Override
     public E peek() {
-        throw new NotYetImplementedException();
+        if (!hasWork()){
+            throw new NoSuchElementException();
+        }
+        return data[0];
     }
 
     @Override
     public E next() {
-        throw new NotYetImplementedException();
+        if (!hasWork()){
+            throw new NoSuchElementException();
+        }
+        E ans = data[0];
+        //replace the empty root
+        int hole = percolateDown(0, data[size - 1]);
+        data[hole] = data[size - 1];
+        size--;
+        return ans;
     }
 
     @Override
     public int size() {
-        throw new NotYetImplementedException();
+        return size;
     }
 
     @Override
     public void clear() {
-        throw new NotYetImplementedException();
+        capacity = 22;
+        data = (E[])new Object[capacity];
+        size = 0;
     }
+
+    private int percolateDown(int hole, E val) {
+        // 0 indexed 4 heap
+        int child = 4 * hole + 1;
+        while (child < size) {
+            int minChild = child;
+            for (int i = 1; i < 4; i++) {
+                int nextChild = child + i;
+                if (nextChild < size && compareType.compare(data[nextChild], data[minChild]) < 0) {
+                    minChild = nextChild;
+                }
+            }
+            if (compareType.compare(data[minChild], val) < 0) {
+                data[hole] = data[minChild];
+                hole = minChild;
+                child = 4 * hole + 1;
+            } else {
+                break;
+            }
+        }
+        return hole;
+    }
+
+    private int percolateUp(int hole, E val){
+        // 0 indexed 4 heap
+        while (hole > 0 && compareType.compare(val, data[(hole-1)/4]) < 0){
+            data[hole] = data[(hole-1)/4];
+            hole = (hole-1)/4;
+        }
+        return hole;
+    }
+
+    /*
+    private class HeapSort implements Comparator<MinFourHeap> {
+        @Override
+        public int compare(MinFourHeap o1, MinFourHeap o2) {
+            return 0;
+        }
+    }
+
+    private class QuickSort implements Comparator<MinFourHeap> {
+        @Override
+        public int compare(MinFourHeap o1, MinFourHeap o2) {
+            return 0;
+        }
+    }
+
+    private class TopKSort implements Comparator<MinFourHeap> {
+        @Override
+        public int compare(MinFourHeap o1, MinFourHeap o2) {
+            return 0;
+        }
+    }
+
+     */
 }
