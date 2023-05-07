48298066c2d327de88546cd0e42b410659042c69
Ethan <ebai2022@cs.washington.edu>
2023-05-06 18:28:51 -0700

    passed quicksort fuzz

--- src/main/java/p2/sorts/QuickSort.java
+++ src/main/java/p2/sorts/QuickSort.java
@@ -11,68 +11,44 @@ public class QuickSort {
     }
 
     public static <E> void sort(E[] array, Comparator<E> comparator) {
-        // pick pivot out of 3 (arr[lo] arr[hi-lo] arr[hi-lo / 2])
-        // partition data into elements less than the pivot (A), the pivot (B),
-        // and elements greater than the pivot (C)
-        // recursively sort A and C
         partition(array, 0, array.length - 1, comparator);
-
     }
 
-    /*
-    public static <E> void quicksort(E[] arr, int lo, int hi, Comparator<E> c){
-        if (lo < hi){
-            int pivot = pickPivot(arr, lo, hi, c);
-            //partition(arr, lo, pivot, hi, c);
-            quicksort(arr, lo, pivot - 1, c);
-            quicksort(arr, pivot + 1, hi, c);
-        }
-    }
-     */
-
-    public static <E> int pickPivot (E[] arr, int lo, int hi, Comparator<E> c){
-        int mid = (hi + lo) / 2;
-        if (mid == lo){
-            if (c.compare(arr[lo], arr[hi]) < 0) {
-                return hi;
-            } else{
-                return lo;
+    private static <E> void partition(E[] array, int lo, int hi, Comparator<E> comparator) {
+        if (lo < hi) {
+            int pivotIndex = pickPivot(array, lo, hi, comparator);
+            E pivotValue = array[pivotIndex];
+            swap(array, pivotIndex, hi);
+            int newPivot = lo;
+            for (int i = lo; i < hi; i++) {
+                if (comparator.compare(array[i], pivotValue) < 0) {
+                    swap(array, i, newPivot);
+                    newPivot++;
+                }
             }
+            swap(array, newPivot, hi);
+            partition(array, lo, newPivot - 1, comparator);
+            partition(array, newPivot + 1, hi, comparator);
         }
-        if ((c.compare(arr[lo], arr[mid]) < 0 && c.compare(arr[lo], arr[hi]) > 0) ||
-                (c.compare(arr[lo], arr[mid]) >= 0 && c.compare(arr[lo], arr[hi]) <= 0)){
-            return lo;
-        } else if ((c.compare(arr[mid], arr[lo]) < 0 && c.compare(arr[mid], arr[hi]) > 0) ||
-                (c.compare(arr[mid], arr[lo]) > 0 && c.compare(arr[mid], arr[hi]) < 0)){
-            return mid;
-        }
-        return hi;
     }
 
-    public static <E> void partition(E[] arr, int lo, int hi, Comparator<E> comparator){
-        if (lo >= hi){
-            return;
+    private static <E> int pickPivot(E[] array, int lo, int hi, Comparator<E> comparator) {
+        int mid = (lo + hi) / 2;
+        if (comparator.compare(array[lo], array[mid]) > 0) {
+            swap(array, lo, mid);
         }
-        int pivot = pickPivot(arr, lo, hi, comparator);
-        E pivotVal = arr[pivot];
-        arr[pivot] = arr[lo];
-        arr[lo] = pivotVal;
-        int i = lo + 1;
-        int j = hi;
-        while (i < j){
-            if (comparator.compare(pivotVal, arr[j]) < 0){
-                j--;
-            } else if (comparator.compare(arr[i], pivotVal) <= 0){
-                i++;
-            } else{
-                E temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+        if (comparator.compare(array[lo], array[hi]) > 0) {
+            swap(array, lo, hi);
         }
-        arr[lo] = arr[i];
-        arr[i] = pivotVal;
-        partition(arr, lo, i - 1, comparator);
-        partition(arr, j + 1, hi, comparator);
+        if (comparator.compare(array[mid], array[hi]) > 0) {
+            swap(array, mid, hi);
+        }
+        return mid;
+    }
+
+    private static <E> void swap(E[] array, int i, int j) {
+        E temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
--- src/test/java/provided/QuickSortTests.java
+++ src/test/java/provided/QuickSortTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import p2.sorts.QuickSort;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -29,6 +31,32 @@ public class QuickSortTests {
 		QuickSort.sort(arr, Integer::compareTo);
 		for(int i = 0; i < arr.length; i++) {
 			assertEquals(arr[i], arr_sorted[i]);
+		}
+	}
+
+	@Test()
+	@Timeout(value = 3000, unit = TimeUnit.MILLISECONDS)
+	public void fuzztest(){
+		for (int i = 0; i < 100; i++) {
+			// Generate a random array of integers
+			Integer[] arr = new Integer[new Random().nextInt(500)];
+			Random rand = new Random();
+			for (int j = 0; j < arr.length; j++) {
+				arr[j] = rand.nextInt(500); // generate integers between -1000 and 1000
+			}
+			// Make a copy of the array
+			Integer[] arrCopy = arr.clone();
+			// Sort the array using quicksort
+			QuickSort.sort(arr, Integer::compareTo);
+			// Sort the copy using Java's built-in Arrays.sort() function
+			Arrays.sort(arrCopy);
+			// Check that the two arrays are the same
+			int index = 0;
+			for (Integer integer : arr){
+				System.out.println(integer + " " + arrCopy[index]);
+				assertEquals(integer, arrCopy[index], index);
+				index++;
+			}
 		}
 	}
 }
